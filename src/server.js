f5ffd86861ece4e61e8359ef658f460a167f72e5
unknown <joaopaulozinga@gmail.com>
2020-06-02 05:09:38 +0100

    Conexão com o banco de Dados Mongo Atlas

--- src/server.js
+++ src/server.js
@@ -1,13 +1,24 @@
 
+require('dotenv').config();
+
 const express = require("express");
-
 const app = express();
-const port = 3333;
 
-app.get("/", (req, res) => {
-  res.send("Hello word");
-});
+const {MongoClient} = require('mongodb');
 
-app.listen(port, () => {
-  console.log(`Servidor inicializado na porta ${port}`);
-});
+const client = new MongoClient(process.env.MONGO_URI, { useUnifiedTopology: true, useNewUrlParser: true});
+
+async function connectDB(){
+  try {
+    await client.connect().then(() => {
+      console.error(`Conexão com o banco de dados efectuada com sucesso!`)
+    });
+  }
+  catch (e) { console.error(`Erro ao conectar ao Banco de dados:\n ${e}`);}
+}
+
+connectDB();
+
+app.get("/", (req, res) => { res.send("Hello word") });
+
+app.listen(process.env.PORT, () => console.log(`Servidor inicializado na porta ${process.env.PORT}`) );
